75465ade3588a52208cc6eae56c5725a71c7f25c
Fabrisio-Huayllas <fabrisio.huayllas@alumnos.ucn.cl>
2025-09-19 18:54:21 -0300

    feat: add migations, create DataCOntext, DesingTimeDBContextFactory.Cs, and create app.db

--- Program.cs
+++ Program.cs
@@ -1,13 +1,52 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using TiendaProyecto.src.Domain.Models;
+using TiendaProyecto.src.Infrastructure.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("SqliteDatabase") ?? throw new InvalidOperationException("Connection string SqliteDatabase no configurado");
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Add services to the container
+builder.Services.AddOpenApi();
+
+#region Identity Configuration
+Log.Information("Configurando Identity");
+builder.Services.AddIdentityCore<User>(options =>
+{
+    //Configuración de contraseña
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+
+    //Configuración de Email
+    options.User.RequireUniqueEmail = true;
+
+    //Configuración de UserName
+    options.User.AllowedUserNameCharacters = builder.Configuration["IdentityConfiguration:AllowedUserNameCharacters"] ?? throw new InvalidOperationException("Los caracteres permitidos para UserName no están configurados.");
+})
+.AddRoles<Role>()
+.AddEntityFrameworkStores<DataContext>()
+.AddDefaultTokenProviders();
+#endregion
 
 // Configuración de logging (Serilog)
+#region Logging Configuration
 builder.Host.UseSerilog((context, services, configuration) => configuration
     .ReadFrom.Configuration(context.Configuration)
     .ReadFrom.Services(services));
+#endregion
 
-// Add services to the container
-builder.Services.AddOpenApi();
+
+
+// region Database Configuration
+#region Database Configuration
+Log.Information("Configurando base de datos SQLite");
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(connectionString));
+#endregion
 
 var app = builder.Build();
 
